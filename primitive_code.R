5392ecad87bb75eeb02d1d08f01273adc1ac3bbc
YasserZhang <saasbook@saasbook.(none)>
2014-08-18 17:04:36 -0700

    second update

--- primitive_code.R
+++ primitive_code.R
@@ -4,7 +4,10 @@
 [9] "Ticket"      "Fare"        "Cabin"       "Embarked"   
 
 getwd()
+# windows working directory
 setwd("~/R_Projects/Titantic")
+# ubuntu working directory
+setwd("~/R_Projects/Titanic")
 getwd()
 
 train = read.csv("train.csv", stringsAsFactor = FALSE)
@@ -12,3 +15,14 @@ test = read.csv("test.csv", stringsAsFactor = FALSE)
 
 dim(train)
 head(train)
+
+library(rpart)
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+fancyRpartPlot(fit)
+
+class(train$Name)
+train$Name[1]
