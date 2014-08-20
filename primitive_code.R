b13d55e3e29366925b583cc921fc4726f2ffc5aa
YasserZhang <saasbook@saasbook.(none)>
2014-08-19 17:13:28 -0700

    finish the rest part of manipulating names and titles

--- primitive_code.R
+++ primitive_code.R
@@ -29,14 +29,12 @@ train$Child <- 0
 train$Child[train$Age < 18] <- 1
 aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
 
-# explore the ticket fare's influence on survival
+# explore the ticket fare's influence on survival and also Pcalss and sex
 train$Fare2 <- '30+'
 train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
 train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
 train$Fare2[train$Fare < 10] <- '<10'
 aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
-
-
 
 # using decision tree
 library(rpart)
@@ -54,7 +52,12 @@ fancyRpartPlot(new.fit)
 ### on the nodes that you want to kill. Once you’re satisfied with the tree, hit ‘quit’ and it will
 ### be stored to the new.fit object. Try to look for overly complex decisions being made, and kill 
 ### the nodes that appear to go to far.
-
+# predict Survival or not to test data based on fit
+Prediction <- predict(fit, test, type = "class")
+# build a new data frame that only include PassengerId and Survived for the use of submission
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+# write the submit data into a csv file
+write.csv(submit, file = "myfirstdtree.csv", row.names = FALSE)
 
 # manipulate text information
 class(train$Name)
@@ -95,22 +98,43 @@ combi$Title <- factor(combi$Title)
 combi$FamilySize <- combi$SibSp + combi$Parch + 1
 # extract each passenger's last name, which is the first part in the name column
 combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+head(combi$Surname)
+# combine FamilySize and Surname to build a new column FamilyID
+combi$FamilyID <- paste(as.character(combi$FamilySize), combi$Surname, sep="")
+head(combi$FamilyID)
+# check out an example of FamilyID
+combi[combi$FamilyID == "3Brown",]
 
+# build a new data frame holding the table of FamilyID
+famIDs <- data.frame(table(combi$FamilyID))
+# customize the famIDs data frame to a new one which only holds rows whose Var1 frequency is less than or equal to 2
+famIDs <- famIDs[famIDs$Freq <= 2,]
+# reset values to the FamilyID in the combi data frame, setting each row's FamilyID's value to "small" if its FamilyID can be found in famIDs$Var1.
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1]  <- "small"
+# change the class of FamilyID to factor
+combi$FamilyID <- factor(combi$FamilyID)
+# To this point, we finish the manipulation on names and titles.
+#########################
 
-
-
-
-
-
-
-
-
-# split combi and bring back the train data with a new column "Title"
+# split combi and bring back the train data
 train1  <- combi[1:891,]
 # see the relation between the title and survival rate
 table(train1$Survived)
 table(train1$Title)
 table(train1$Title, train1$Survived)
+# split combi and bring back the test data
+test <- combi[892:1309,]
+# build a new decision tree based on existing columns and others that we just generate
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+             data=train, method="class")
+# predict Survival or not to test data based on fit
+Prediction <- predict(fit, test, type = "class")
+# build a new data frame that only include PassengerId and Survived for the use of submission
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+# write the submit data into a csv file
+write.csv(submit, file = "myfirstdtree.csv", row.names = FALSE)
+
+
 
 
 data.frame(SibSp = train$SibSp, Parch = train$Parch)
